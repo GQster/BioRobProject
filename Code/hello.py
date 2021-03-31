50ab8af9a703f19d30db8eee48d53d0e6c3d85b4
grantcroft <grantcroft.25@gmail.com>
2021-03-31 02:37:00 +0000

    update

--- Code/hello.py
+++
@@ -1,2 +0,0 @@
-def my_function(n):
-    print("Hello "+n+" from a function")
--- Code/main.py
+++ Code/main.py
@@ -4,10 +4,13 @@
 #pyenv install 3.7.9
 import numpy as np
 import pandas as pd
+from IPython.display import display     #for display
 
 from hello import *             #imports entire file
 #from hello import my_function  #imports sepcific function
 
-name = "bob"
 
-my_function(name)
+
+experiment_data_1066528_filtered = pd.read_csv('Data/1066528_heartrate_filtered.csv')
+
+display(experiment_data_1066528_filtered)
