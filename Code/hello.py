ab70ca97adbb3f914b5dcee09a21b23d3de08091
grantcroft <grantcroft.25@gmail.com>
2021-03-31 01:34:08 +0000

    updates

---
+++ Code/hello.py
@@ -0,0 +1,2 @@
+def my_function(name):
+  print("Hello " + name +" from a function")
---
+++ Code/main.py
@@ -0,0 +1,9 @@
+#run this line:
+#pyenv version
+#if not python 3.7.9
+#pyenv install 3.7.9
+
+import hello
+
+name = bob
+my_function(name)
