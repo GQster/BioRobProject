c5ae3340e9a340d6f142918e2104875bbca9b9cb
PatrickSantorellaDoyle <pxs5558@rit.edu>
2021-04-01 21:34:26 +0000

    fixes

--- Code/main.py
+++ Code/main.py
@@ -5,21 +5,13 @@
 #pyenv install 3.7.9
 import numpy as np
 import pandas as pd
-import xlrd						        # So can read the spreadsheet IF IS A .XLS FILE
 from IPython.display import display     #for display
 
 #from hello import *             #imports entire file
 #from hello import my_function  #imports sepcific function
 
 
-#workbook = xlrd.open_workbook('Monthly_Data_Input.xls')	#opens the spreadsheet in python
-#worksheetINFO = workbook.sheet_by_index(0)			        #opens the first worksheet in the spreadsheet
 
+df = pd.read_excel("Data/1066528_heartrate.xlsx")
+print(df)
 
-experiment_data_1066528_filtered = pd.read_csv('Data/1066528_heartrate_filtered.csv')
-
-display(experiment_data_1066528_filtered)
-
-print(experiment_data_1066528_filtered[0])
-print('applesauce')
-print('applesauce2')
