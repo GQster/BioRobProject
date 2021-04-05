d66d096fa39078a2c9486b3189d869eabfcd5f82
grantcroft <grantcroft.25@gmail.com>
2021-04-05 01:24:23 +0000

    change the preprocessor to a function

--- Code/main.py
+++ Code/main.py
@@ -11,29 +11,41 @@ from hmmlearn import hmm                #for the hidden markov model
 #from hello import *             #imports entire file
 #from hello import my_function  #imports sepcific function
 
+sleep_file = "Data/1066528_labeled_sleep.csv"
+HR_file = "Data/1066528_heartrate.csv"
 
-df_sleep = pd.read_csv("Data/1066528_labeled_sleep.csv")
-df_sleep.columns =['TimeSec', 'SleepLVL']
-df_hr = pd.read_csv("Data/1066528_heartrate.csv")
-df_hr.drop_duplicates()
-df_hr.columns =['TimeSec', 'HR']
-#display(df_sleep)
-#display(df_hr)
 
-temp_df = df_sleep.iloc[:,0]
+def Sleep_HR_data_processor(sleep_file, HR_file, fileNumber):        #files as .csv. fileNumber is the xth time function is called (used to name the files differently)
 
-df_Sleep_HR = df_sleep                              #coppies sleep time column and data column
-df_Sleep_HR["HR"] = 0                               #adds a column named HR
+    df_sleep = pd.read_csv(sleep_file)
+    df_sleep.columns =['TimeSec', 'SleepLVL']
+    df_hr = pd.read_csv(HR_file)
+    df_hr.drop_duplicates()
+    df_hr.columns =['TimeSec', 'HR']
 
-for x in range(len(temp_df)):
-    #display(x)
-    search_value = temp_df.iloc[x]                  #df.loc[row,column]
-    result_index = df_hr['TimeSec'].sub(search_value).abs().idxmin()    # returns row of hr data
-    df_Sleep_HR.iloc[x, 2] = df_hr.iloc[result_index, 1]      #copy desired data to temp_df
-    display(df_hr.iloc[result_index, 1])
-display(df_Sleep_HR)
-df_Sleep_HR.to_csv('Sleep_HR.csv')
-#df_Sleep_HR.columns =['TimeSec', 'SleepLVL', 'HR']  #Creates line labels 
+    #temp_df = df_sleep.iloc[:,0]                        #used to loop though the DF 
+    df_Sleep_HR = df_sleep                              #coppies sleep time column and data column
+    df_Sleep_HR["HR"] = 0                               #adds a column named HR
+
+    for x in range(len(df_sleep)):
+        #display(x)
+        search_value = df_sleep.iloc[x, 0]                  #df.iloc[row,column]
+        result_index = df_hr['TimeSec'].sub(search_value).abs().idxmin()    # returns row of hr data
+        #display(df_hr.iloc[result_index, 1])           #displays the value found
+        df_Sleep_HR.iloc[x, 2] = df_hr.iloc[result_index, 1]      #copy desired data to temp_df
+    df_Sleep_HR.to_csv('Data/Check/Sleep_HR_{}.csv'.format(fileNumber))                  #saves as a .csv 
+    return df_Sleep_HR
+
+
+processed_data = Sleep_HR_data_processor(sleep_file, HR_file, 1) 
+display(processed_data)
+
+
+
+
+
+
+
 
 
 #Hidden Mrkov Model
