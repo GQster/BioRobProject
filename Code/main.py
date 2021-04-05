cd84bd9043c9b52c648c8c3065a379c4e3433dde
grantcroft <grantcroft.25@gmail.com>
2021-04-05 01:10:56 +0000

    uypdates

--- Code/main.py
+++ Code/main.py
@@ -12,28 +12,28 @@ from hmmlearn import hmm                #for the hidden markov model
 #from hello import my_function  #imports sepcific function
 
 
-
-
 df_sleep = pd.read_csv("Data/1066528_labeled_sleep.csv")
 df_sleep.columns =['TimeSec', 'SleepLVL']
 df_hr = pd.read_csv("Data/1066528_heartrate.csv")
 df_hr.drop_duplicates()
 df_hr.columns =['TimeSec', 'HR']
-display(df_sleep)
+#display(df_sleep)
 #display(df_hr)
 
 temp_df = df_sleep.iloc[:,0]
-#display(temp_df)
-df_Sleep_HR = df_sleep                      #coppies sleep time column and data column
 
-for x in temp_df:
-    search_value = temp_df[x]               #df.loc[row,column]
-   # print(search_value)
+df_Sleep_HR = df_sleep                              #coppies sleep time column and data column
+df_Sleep_HR["HR"] = 0                               #adds a column named HR
+
+for x in range(len(temp_df)):
+    #display(x)
+    search_value = temp_df.iloc[x]                  #df.loc[row,column]
     result_index = df_hr['TimeSec'].sub(search_value).abs().idxmin()    # returns row of hr data
-    df_Sleep_HR[x, 2] = df_hr[result_index, 1]  #copy desired data to temp_df
-    display(result_index)
-#display(result_index)
-df_Sleep_HR.columns =['TimeSec', 'SleepLVL', 'HR']#Creates line labels 
+    df_Sleep_HR.iloc[x, 2] = df_hr.iloc[result_index, 1]      #copy desired data to temp_df
+    display(df_hr.iloc[result_index, 1])
+display(df_Sleep_HR)
+df_Sleep_HR.to_csv('Sleep_HR.csv')
+#df_Sleep_HR.columns =['TimeSec', 'SleepLVL', 'HR']  #Creates line labels 
 
 
 #Hidden Mrkov Model
