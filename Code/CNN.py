72f0f6d4e2bcd5d890785591a97d08891f5dd9c0
grantcroft <grantcroft.25@gmail.com>
2021-04-28 21:20:47 +0000

    updates to CNN

--- Code/CNN.py
+++ Code/CNN.py
@@ -1,13 +1,14 @@
 #from functions import *     #imports our functions
 from functions import *     #imports our functions
 
-
+#this Def needs to be in the CNN file
 def evalModel(epochs = 10, verbose = 0):
     # fit model
     history = model.fit(trainX, trainy, validation_data=(testX, testy), epochs=epochs, verbose=verbose)
     # evaluate the model
     train_mse = model.evaluate(trainX, trainy, verbose=0)
     test_mse = model.evaluate(testX, testy, verbose=0)
+    print("Loss Values:")
     print('Train: %.3f, Test: %.3f' % (train_mse, test_mse))
     return history #end of evalModel
 
@@ -34,16 +35,39 @@ trainy, testy = np.array_split(y,[n_train])
 #display("TestX", testX)
 #display("Testy", testy)
 
-
+def modelTest():
 # define model
-model = Sequential()
-model.add(Dense(64, input_dim=1, activation='relu', kernel_initializer='he_uniform'))
-model.add(Dense(32, activation='tanh'))
-model.add(Dense(6, activation='linear'))
-opt = SGD(lr=0.01, momentum=0.9)
-model.compile(loss='mean_squared_logarithmic_error', optimizer=opt)
+    model = Sequential()
+    model.add(Dense(64, input_dim=1, activation='relu', kernel_initializer='he_uniform'))
+    model.add(Dense(32, activation='tanh'))
+    model.add(Dense(6, activation='linear'))
+    opt = SGD(lr=0.01, momentum=0.9)
+    model.compile(loss='mean_squared_logarithmic_error', optimizer=opt)
 
-evalModel(epochs = 5, verbose = 1)
+    #evalModel(epochs = 5, verbose = 1)
+
+    # fit model
+    history = model.fit(trainX, trainy, validation_data=(testX, testy), epochs=2, verbose=0)
+    # evaluate the model
+    train_mse = model.evaluate(trainX, trainy, verbose=0)
+    test_mse = model.evaluate(testX, testy, verbose=0)
+    #print("Loss Values:")
+    print('Train: %.3f, Test: %.3f' % (train_mse, test_mse))
+    return train_mse, test_mse
+train = 0
+test = 0
+n = 5
+for x in range(n):
+    traintemp, testtemp = modelTest()
+    train +=traintemp
+    test += testtemp
+print('AVG: Train: %.3f, Test: %.3f' % (train/n, test/n))
+
+
+
+
+
+
 
 
 #print("plots")
